b46d159df6cb51c9753f82e71675ce4d88fa0366
Juan Suárez <juan@Juans-MacBook-Pro.local>
2024-01-31 16:41:54 +0100

    feat(login): add login layout, left back

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,28 @@ import { Stack, useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { TouchableOpacity } from "react-native";
+import * as SecureStore from "expo-secure-store";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+
+const CLERK_PUBLISHABLE_KEY: string =
+  "pk_test_Y3Jpc3AtbGFtcHJleS00NC5jbGVyay5hY2NvdW50cy5kZXYk";
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (error) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (error) {
+      return null;
+    }
+  },
+};
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -40,11 +62,22 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+      <RootLayoutNav />
+    </ClerkProvider>
+  );
 }
 
 function RootLayoutNav() {
   const router = useRouter();
+  const { isLoaded, isSignedIn } = useAuth();
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push("/(modals)/login");
+    }
+  }, [isLoaded]);
 
   return (
     <Stack>
@@ -55,6 +88,22 @@ function RootLayoutNav() {
           title: "Log in or sign up",
           headerTitleStyle: { fontFamily: "mon" },
           presentation: "modal",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28}></Ionicons>
+            </TouchableOpacity>
+          ),
+        }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="listing/[id]"
+        options={{ headerTitle: "" }}
+      ></Stack.Screen>
+      <Stack.Screen
+        name="(modals)/booking"
+        options={{
+          presentation: "transparentModal",
+          animation: "fade",
           headerLeft: () => (
             <TouchableOpacity onPress={() => router.back()}>
               <Ionicons name="close-outline" size={28}></Ionicons>
