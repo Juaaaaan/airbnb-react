9758f6392c280ae6c093cf7eb62092a82c493838
Juan Suárez <juan@Juans-MacBook-Pro.local>
2024-02-26 14:25:32 +0100

    feat(profile): add image

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,22 +1,124 @@
-import { View, Button, Text } from "react-native";
-import React from "react";
-import { useAuth } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
+import {
+  View,
+  Button,
+  Text,
+  StyleSheet,
+  ScrollView,
+  RefreshControl,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useAuth, useUser } from "@clerk/clerk-expo";
+import { Link, router } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { defaultStyles } from "@/constants/Styles";
 
 const Page = () => {
   const { signOut, isSignedIn } = useAuth();
 
-  return (
-    <View>
-      <Button title="log out" onPress={() => signOut()} />
+  const signIn = () => {
+    router.navigate("/(modals)/login");
+  };
 
-      {!isSignedIn && (
-        <Link href={"/(modals)/login"}>
-          <Text>Log In</Text>
-        </Link>
-      )}
-    </View>
+  const { user } = useUser();
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [LastName, setLastName] = useState(user?.lastName);
+  const [email, setEmail] = useState(user?.emailAddresses[0].emailAddress);
+
+  const [edit, setEdit] = useState(false);
+
+  useEffect(() => {
+    if (!user) return;
+
+    setFirstName(user.firstName);
+    setLastName(user.lastName);
+    setEmail(user.emailAddresses[0].emailAddress);
+  }, [user]);
+
+  const onSaveUser = async () => {};
+
+  const onCaptureImage = async () => {};
+
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <SafeAreaView style={defaultStyles.container}>
+      <View style={styles.headerComponent}>
+        <Text style={styles.header}>Profile</Text>
+        <Ionicons
+          name="notifications-outline"
+          size={26}
+          color={Colors.dark}
+        ></Ionicons>
+      </View>
+
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        {user && (
+          <View style={styles.card}>
+            <TouchableOpacity onPress={onCaptureImage}>
+              <Image
+                source={{ uri: user.imageUrl }}
+                style={styles.avatar}
+              ></Image>
+            </TouchableOpacity>
+          </View>
+        )}
+      </ScrollView>
+
+      {isSignedIn && <Button title="log out" onPress={() => signOut()} />}
+      {!isSignedIn && <Button title="log in" onPress={() => signIn()} />}
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  headerComponent: {
+    flexDirection: "row",
+    padding: 24,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  header: {
+    fontFamily: "mon",
+    fontSize: 24,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 24,
+    borderRadius: 16,
+    marginHorizontal: 24,
+    marginTop: 24,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    alignItems: "center",
+    gap: 14,
+    marginBottom: 24,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+});
 
 export default Page;
